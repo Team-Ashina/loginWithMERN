3e28330c71290c38b493e46bcb42906b1ef76395
IvanAiOr <irivascas@gmail.com>
2020-09-15 00:16:41 -0400

    Changes on register and design on login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Form, Button, InputGroup } from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 export const Login = () =>
 {
 	return (
-		<div>
-			<h1>Hola</h1>
-		</div>
+		<Fragment>
+			<div className="auth__form-container">
+				<h2 className="auth__title">Log in to your account</h2>
+				<Form className="auth__form">
+					<Form.Label>Email address</Form.Label>
+					<InputGroup className="mb-4">
+
+						<InputGroup.Prepend>
+							<InputGroup.Text>@</InputGroup.Text>
+						</InputGroup.Prepend>
+
+                        <Form.Control 
+                            required
+							type="email"
+							placeholder="Email address"
+						/>
+
+					</InputGroup>
+
+					<Form.Label>Password</Form.Label>
+					<InputGroup className="mb-2">
+
+						<InputGroup.Prepend>
+							<InputGroup.Text>Æ</InputGroup.Text>
+						</InputGroup.Prepend>
+
+                        <Form.Control 
+                            required
+							type="password"
+							placeholder="Password"
+						/>
+
+					</InputGroup>
+
+					<Button className="btn-action" type="submit">
+						Log In
+					</Button>
+				</Form>
+				<div className="auth__footer">
+                    You do haven't an account?
+					<Link to="/auth/register">
+						<span> Sign in</span>
+					</Link>
+				</div>
+			</div>
+		</Fragment>
 	)
 }
